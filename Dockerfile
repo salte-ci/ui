e068573166f970c8e96a1a19c6c319e80fc16bb2
Nick Woodward <nick@salte.io>
2019-12-13 13:20:30 -0600

    ci: deploy to aws

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM nginx:alpine
-
-COPY conf.d/default.conf /etc/nginx/conf.d/default.conf
-COPY dist /site
--- main.tf
+++ main.tf
@@ -1,11 +1,3 @@
-variable "DO_DOCKER_CA_CERT" {}
-variable "DO_DOCKER_CLIENT_CERT" {}
-variable "DO_DOCKER_CLIENT_KEY" {}
-variable "REGISTRY" {}
-variable "REGISTRY_USER" {}
-variable "REGISTRY_PASSWORD" {}
-variable "IMAGE" {}
-
 terraform {
   backend "s3" {
     bucket = "salte-ci"
@@ -15,53 +7,137 @@ terraform {
 }
 
 locals {
-  environment = "${terraform.workspace}"
+  environment = terraform.workspace
   domain_name = "${local.environment == "live" ? "salte.ci" : "${local.environment}.salte.ci"}"
+  s3_origin_id = "SalteCI-${local.environment}"
 }
 
-# Configure the Docker provider
-provider "docker" {
-  host = "tcp://manager.salte.io:2376/"
+provider "aws" {
+  region = "us-east-1"
+}
 
-  ca_material   = "${base64decode(var.DO_DOCKER_CA_CERT)}"
-  cert_material = "${base64decode(var.DO_DOCKER_CLIENT_CERT)}"
-  key_material  = "${base64decode(var.DO_DOCKER_CLIENT_KEY)}"
+data "aws_iam_policy_document" "policy" {
+  statement {
+    actions   = ["s3:GetObject"]
+    resources = ["${aws_s3_bucket.bucket.arn}/*"]
 
-  registry_auth {
-    address  = "${var.REGISTRY}"
-    username = "${var.REGISTRY_USER}"
-    password = "${var.REGISTRY_PASSWORD}"
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
+  }
+
+  statement {
+    actions   = ["s3:ListBucket"]
+    resources = [aws_s3_bucket.bucket.arn]
+
+    principals {
+      type        = "AWS"
+      identifiers = [aws_cloudfront_origin_access_identity.origin_access_identity.iam_arn]
+    }
   }
 }
 
-resource "docker_service" "default" {
-  name = "${local.environment}-salte-ci"
+resource "aws_s3_bucket_policy" "policy" {
+  bucket = aws_s3_bucket.bucket.id
+  policy = data.aws_iam_policy_document.policy.json
+}
 
-  labels {
-    "traefik.docker.network"       = "dmz"
-    "traefik.enable"               = "true"
-    "traefik.frontend.rule"        = "Host:${local.domain_name}"
-    "traefik.port"                 = 80
-    "traefik.protocol"             = "http"
-    "traefik.frontend.entryPoints" = "https"
+resource "aws_s3_bucket" "bucket" {
+  bucket = "salte-ci-${local.environment}"
+  acl    = "private"
+
+  tags = {
+    Name        = "Salte CI"
+    Environment = "${local.environment}"
+  }
+}
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "Salte CI - ${local.environment}"
+}
+
+data "aws_acm_certificate" "cert" {
+  domain   = "*.salte.ci"
+  statuses = ["ISSUED"]
+}
+
+data "aws_route53_zone" "salte_ci" {
+  name         = "salte.ci."
+  private_zone = false
+}
+
+resource "aws_route53_record" "site" {
+  zone_id = data.aws_route53_zone.salte_ci.zone_id
+  name    = local.domain_name
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.s3_distribution.domain_name
+    zone_id                = aws_cloudfront_distribution.s3_distribution.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_cloudfront_distribution" "s3_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.bucket.bucket_regional_domain_name
+    origin_id   = local.s3_origin_id
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
   }
 
-  task_spec {
-    container_spec {
-      image = "${var.IMAGE}"
+  enabled             = true
+  is_ipv6_enabled     = true
+  default_root_object = "index.html"
+
+  aliases = [local.domain_name]
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD", "OPTIONS"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = local.s3_origin_id
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
     }
 
-    restart_policy {
-      condition = "any"
-      delay     = "5s"
-    }
-
-    networks = ["dmz"]
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
   }
 
-  mode {
-    replicated {
-      replicas = 1
+  custom_error_response {
+    error_code = 404
+    response_code = 200
+    response_page_path = "/index.html"
+  }
+
+  price_class = "PriceClass_100"
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
     }
   }
+
+  tags = {
+    Environment = local.environment
+  }
+
+  viewer_certificate {
+    ssl_support_method = "sni-only"
+    acm_certificate_arn = data.aws_acm_certificate.cert.arn
+  }
+}
+
+output "distribution_id" {
+  value = aws_cloudfront_distribution.s3_distribution.id
 }
